6ae88ff46f65e928fedc8b8bca8105f516504524
Alessio Koci <ko.alessio@gmail.com>
2018-04-24 10:56:20 +0200

    Added Obj type

---
+++ fields/types/obj/ObjFilter.js
@@ -0,0 +1,8 @@
+
+/*
+TODO: Not sure what this should look like yet,
+      it's currently basically a copy of the Boolean filter
+      so that the Admin UI builds successfully
+*/
+
+module.exports = require('../boolean/BooleanFilter');
