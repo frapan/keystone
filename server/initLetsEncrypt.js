3390436005bbd77d61644b94e79dc851f8c8e225
Francesco Panciroli <francesco.panciroli@neriwolff.com>
2019-05-03 15:58:07 +0200

    Fatto in modo che letsencrypt non venga richiamato all'avvio dell'applicazione se non configurato in keystone

--- server/initLetsEncrypt.js
+++ server/initLetsEncrypt.js
@@ -6,10 +6,7 @@
  * @api private
  */
 
-var letsencrypt = require('letsencrypt-express');
-
 module.exports = function (keystone, app) {
-
 	var options = keystone.get('letsencrypt');
 	var ssl = keystone.get('ssl');
 	if (!options) {
@@ -19,7 +16,9 @@ module.exports = function (keystone, app) {
 		console.error('Ignoring `letsencrypt` setting because `ssl` is not set.');
 	}
 	if (ssl === 'only') {
-		console.error('To use Let\'s Encrypt you need to have a regular HTTP listener as well. Please set ssl to either `true` or `"force"`.');
+		console.error(
+			'To use Let\'s Encrypt you need to have a regular HTTP listener as well. Please set ssl to either `true` or `"force"`.'
+		);
 	}
 
 	var email = options.email;
@@ -31,10 +30,13 @@ module.exports = function (keystone, app) {
 		approveDomains = [approveDomains];
 	}
 	if (!(agreeTos && email && approveDomains)) {
-		console.error("For auto registation with Let's Encrypt you have to agree to the TOS (https://letsencrypt.org/repository/) (tos: true), provide domains (domains: ['mydomain.com', 'www.mydomain.com']) and a domain owner email (email: 'admin@mydomain.com')");
+		console.error(
+			"For auto registation with Let's Encrypt you have to agree to the TOS (https://letsencrypt.org/repository/) (tos: true), provide domains (domains: ['mydomain.com', 'www.mydomain.com']) and a domain owner email (email: 'admin@mydomain.com')"
+		);
 		return;
 	}
 	// TODO maybe we should use le-store-mongo
+	var letsencrypt = require('letsencrypt-express');
 	var lex = letsencrypt.create({
 		server: server,
 		approveDomains: approveDomains,
